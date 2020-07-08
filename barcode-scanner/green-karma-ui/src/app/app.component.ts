18503a1be9c607289bc58b4885ea93b3ebae37f8
Alejandra Ornelas <aleornelasbarajas@gmail.com>
2020-07-08 17:19:24 -0400

    Adding main layout

--- barcode-scanner/green-karma-ui/src/app/app.component.ts
+++ barcode-scanner/green-karma-ui/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface TabConfig {
+  title: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'green-karma-ui';
-  scanBarCode: boolean = false;
+
+  public tabs: Array<TabConfig> = [
+    { title: 'Search', link: 'search'}, 
+    { title: 'Recycle', link: 'recycle'},
+    { title: 'About', link: 'about'}
+  ];
 }
--- barcode-scanner/green-karma-ui/src/app/app.module.ts
+++ barcode-scanner/green-karma-ui/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { BarecodeScannerLivestreamModule } from 'ngx-barcode-scanner';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarcodeScannerComponent } from './barcode-scanner/barcode-scanner.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { RecycleComponent } from './recycle/recycle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarcodeScannerComponent
+    BarcodeScannerComponent,
+    AboutComponent,
+    SearchComponent,
+    RecycleComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +28,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BarecodeScannerLivestreamModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
