4fd0a2795e8c11aa7f6ec1c9c5a2a456380c4c9e
bhane95 <bintahane95@gmail.com>
2020-07-09 03:28:25 +0000

    Update PR with missing component

--- barcode-scanner/green-karma-ui/src/app/app.component.ts
+++ barcode-scanner/green-karma-ui/src/app/app.component.ts
@@ -33,8 +33,8 @@ export class AppComponent implements OnInit {
   }
 
   getProducts(): void {
-    this.productService.getProducts().subscribe(data => {
+    /*this.productService.getProducts().subscribe(data => {
       this.products = data;
-    })
+    })*/
   }
 }
